07d32c55134c42fd8bc51b407c06f00d4bb9bf81
Bruce0203 <56539682+Bruce0203@users.noreply.github.com>
2022-07-27 15:21:05 +0000

    commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express=require('express');
+const express = require('express')
+const app = express()
+const port = 8080
 
-const port=8080;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const app=express();
-app.get('/',(req,res)=>{
-    res.send("Hello")
-});
-
-app.listen(port);
-console.log("Server is Running");
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
